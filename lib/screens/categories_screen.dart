f3a082fe78e852da6184865e7ea0dc3bed38d846
nejlaban <nejla.bandic@stu.ibu.edu.ba>
2021-02-23 22:35:18 +0100

    Add ListView with categories

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import './category_items_screen.dart';
 
+import 'package:flutter_app/dummy_category_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,10 +15,53 @@ class CategoriesScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'Ovdje ide ListView kategorija',
-        ),
-      ),
+          child: ListView(
+        padding: const EdgeInsets.all(120),
+        children: <Widget>[
+          Container(
+            height: 100,
+            color: Colors.blue[600],
+            child: const Center(
+                child: Text(
+              'Frizerski saloni',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            )),
+          ),
+          SizedBox(height: 30),
+          Container(
+            height: 100,
+            color: Colors.blue[400],
+            child: const Center(
+                child: Text(
+              'Kozmeticki saloni',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            )),
+          ),
+          SizedBox(height: 30),
+          Container(
+            height: 100,
+            color: Colors.blue[200],
+            child: const Center(
+                child: Text(
+              'Wellness',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            )),
+          ),
+          SizedBox(height: 30),
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
         child: Icon(
           Icons.star,
