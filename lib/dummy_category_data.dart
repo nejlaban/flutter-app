3b46d8397a642d6ea4e13aa00c4ea860d9d432a4
elmedinkarisik <edo.karisik1@gmail.com>
2021-02-23 16:56:49 +0100

    Add Category class and create const dummy data

---
+++ lib/dummy_category_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(1, 'Frizerski saloni'),
+  Category(2, 'Restorani'),
+  Category(3, 'Kozmeticki saloni'),
+  Category(4, 'Hoteli'),
+];
---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final int id;
+  final String title;
+
+  const Category(
+    @required this.id,
+    @required this.title,
+  );
+}
