94a854bc48b36a5838fd61c101b7cee404314fa5
nejlaban <nejla.bandic@stu.ibu.edu.ba>
2021-02-25 15:45:55 +0100

    Create CarouselSlider for categories,modified file

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -2,74 +2,99 @@ import 'package:flutter/material.dart';
 
 import './category_items_screen.dart';
 import '../widgets/bottom_navigation_bar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Kategorije',
+        toolbarHeight: 100,
+        title: Container(
+          margin: EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 10.0,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(
+                20.0,
+              ),
+            ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Pretraži kategorije',
+                        hintStyle: TextStyle(color: Colors.white),
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        centerTitle: true,
       ),
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.all(120),
-          children: <Widget>[
-            Container(
-              height: 100,
-              color: Colors.blue[600],
-              child: const Center(
-                child: Text(
-                  'Frizerski saloni',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
+      body: ListView(
+        children: [
+          CarouselSlider(
+            items: [
+              Container(
+                margin: EdgeInsets.all(25.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://media-cdn.tripadvisor.com/media/photo-s/10/f4/49/ef/hair-salon.jpg"),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 100,
-              color: Colors.blue[400],
-              child: const Center(
-                child: Text(
-                  'Kozmeticki saloni',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
+
+              //2nd Image of Slider
+              Container(
+                margin: EdgeInsets.all(25.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://i.ytimg.com/vi/QZbuj3RJcjI/maxresdefault.jpg"),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 100,
-              color: Colors.blue[200],
-              child: const Center(
-                child: Text(
-                  'Wellness',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
+              Container(
+                margin: EdgeInsets.all(25.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://www.romania-insider.com/sites/default/files/styles/article_large_image/public/featured_images/dentist-office.jpg"),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
+            ],
+            options: CarouselOptions(
+              height: 180.0,
+              enlargeCenterPage: true,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              viewportFraction: 0.8,
             ),
-            SizedBox(
-              height: 30,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(
